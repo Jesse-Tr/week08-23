865357310fc7ca8c54df81e8d7d0db057e94b568
Jesse <jtrujillo707@gmail.comgit config --global user.name jesse>
2023-11-28 15:17:58 -0800

    week08 changes

--- components/AddEvent.jsx
+++ components/AddEvent.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import {Box,Input,Button,Textarea,Stack,Select,useToast,} from "@chakra-ui/react";
 import useAuth from "../hooks/useAuth";
-import { addTodo } from "../api/todo";
+import { addEvent } from "../api/event";
 
-const AddTodo = () => {
+const AddEvent = () => {
     const [title, setTitle] = React.useState("");
     const [description, setDescription] = React.useState("");
     const [status, setStatus] = React.useState("pending");
     const [isLoading, setIsLoading] = React.useState(false);
     const toast = useToast();
     const { isLoggedIn, user } = useAuth();
-    const handleTodoCreate = async () => {
+    const handleEventCreate = async () => {
     if (!isLoggedIn) {
         toast({
-            title: "You must be logged in to create a todo",
+            title: "You must be logged in to create a event",
             status: "error",
             duration: 9000,
             isClosable: true,
@@ -21,24 +21,24 @@ const AddTodo = () => {
         return;
         }
     setIsLoading(true);
-        const todo = {
+        const event = {
         title,
         description,
         status,
         userId: user.uid,
     };
-    await addTodo(todo);
+    await addEvent(event);
     setIsLoading(false);
     setTitle("");
     setDescription("");
     setStatus("pending");
-    toast({ title: "Todo created successfully", status: "success" });
+    toast({ title: "Event created successfully", status: "success" });
     };
     return (
         <Box w="40%" margin={"0 auto"} display="block" mt={5}>
         <Stack direction="column">
         <Input
-        placeholder="Title"
+        placeholder="event"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         />
@@ -62,7 +62,7 @@ const AddTodo = () => {
         </option>
         </Select>
         <Button
-        onClick={() => handleTodoCreate()}
+        onClick={() => handleEventCreate()}
         disabled={title.length < 1 || description.length < 1 || isLoading}
         colorScheme="teal"
         variant="solid"
@@ -73,4 +73,4 @@ const AddTodo = () => {
         </Box>
     );
 };
-export default AddTodo;
+export default AddEvent;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import { Container } from "@chakra-ui/react";
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
+import EventList from "../components/EventList";
+
 export default function Home() {
 return (
 <Container maxW="7xl">
 <Auth />
 <TodoList />
+<EventList />
 </Container>
 );
 }
